18505656a56b5540f75deeed77e8bbac4bf1d4d0
daabiiny <daabiiny@gmail.com>
2023-10-20 15:19:16 +0900

    231020

--- day07/src/loop/Ex09.java
+++ day07/src/loop/Ex09.java
@@ -38,7 +38,7 @@ public class Ex09 {
 		
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
-				if(i == 2 || j == 2) {
+				if(i == 0 || i == 4 || j == 0 || j == 4) {
 					System.out.print("# ");
 				} 
 				else {
@@ -46,6 +46,20 @@ public class Ex09 {
 				}
 			}System.out.println();
 		}System.out.println();
+		
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+//				if(i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1) {
+				if((i + j) % 2 == 0) {
+					System.out.print("# ");
+				} 
+				else {
+					System.out.print("  ");
+				}
+			}System.out.println();
+		}System.out.println();
+		
+		
 		
 	}
 }
